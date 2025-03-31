9b3316bc9c281f1b7110dfcbdd64b336f820a2f0
alenorgue <alenoriegag@gmail.com>
2025-03-31 21:14:00 +0200

    finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // ¡Buena suerte!
+const btnCompute = document.getElementById('btn');
+const bmiResult = document.getElementById('bmi-result');
+
+function calculateBMI(){
+    const inputHeight = Number(document.getElementById('height').value);
+    const inputWeight = Number(document.getElementById('weight').value);
+    const calculatedBMI = inputWeight / ((inputHeight / 100) ** 2);
+    const message = document.getElementById('weight-condition');
+
+    bmiResult.value = calculatedBMI.toFixed(2);
+
+    if (calculatedBMI < 18.5 ) { message.textContent = 'Low weight'; }
+    else if (calculatedBMI > 18.5 && calculatedBMI < 24.9) {
+        message.textContent = 'Normal weight';
+    }
+    else if (calculatedBMI > 24.9 && calculatedBMI < 30) {
+        message.textContent = 'Overweight';
+    } else if (bmiValue >= 30) {
+        weightConditionEl.innerText = "Obesity";
+      }
+}
+
+btnCompute.addEventListener('click', calculateBMI);
